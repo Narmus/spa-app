70b632399d00dafe6b9d859d0c30edb4e49f2b12
Arun <arun.ms@codelynks.com>
2024-03-10 14:39:05 +0530

    SPA-4 - moved auth options to separate auth file

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,37 +1,6 @@
-import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { users } from "../../../../Constants/users";
+import { authOptions } from "@/lib/configs/auth/authOptions";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials: any, req: any) {
-        if (!credentials || !credentials?.username || !credentials?.password) {
-          return null;
-        }
-
-        const user: any = users.find(
-          (item: any) =>
-            item?.username?.toLowerCase() ===
-            credentials?.username?.toLowerCase()
-        );
-
-        if (user?.password === credentials?.password) {
-          return user;
-        } else {
-          return null;
-        }
-      },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET
-};
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
